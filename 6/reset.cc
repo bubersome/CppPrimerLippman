d818ebf0bdb2e6486b46976c182d13c778518fc9
bubersome <bubersome@gmail.com>
2022-08-19 18:15:28 -0500

    Function taking pointer or address

--- 6/reset.cc
+++ 6/reset.cc
@@ -9,14 +9,14 @@ using std::vector;
 // function that takes a reference to an int 
 // and sets the given object to zero
 // i is just another name for the object passed to reset
-void reset(int &i)  
+void resetAddress(int &i) //take address or argument name
 {
     i = 0;  // changes the value of the object to which i refers
 }
 
 // function that takes a pointer 
 // and sets the pointed-to value to zero
-void reset(int *ip)
+void resetPointer(int *ip) //take pointer
 {
     *ip = 0;  // changes the value of the object to which ip points
     ip = 0;   // changes the local copy of ip; the argument is unchanged
@@ -25,16 +25,16 @@ void reset(int *ip)
 int main() 
 {
 	int j = 42;
-	reset(j);  // j is passed by reference; the value in j is changed
+	resetAddress(j);  // j is passed by reference; the value in j is changed
 	cout << "j = " << j  << endl;  // prints j = 0
 
 	j = 42;    // restore the original value of j
-	reset(&j);                     // changes j but not the address of j
+	resetPointer(&j);                     // changes j but not the address of j
 	cout << "j = "  << j << endl;  // prints j = 0
 
 	j = 42;    // restore the original value of j
 	int *p = &j;
-	reset(p); // changes object to which p points not the address in p
+	resetPointer(p); // changes object to which p points not the address in p
 	cout << "j = "  << *p << endl;  // prints j = 0
 
     return 0;
