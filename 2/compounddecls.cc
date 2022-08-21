1a1965cab9d2bae0f7ed3a0b8eda5c2d6ea22907
bubersome <bubersome@gmail.com>
2022-08-20 20:03:54 -0500

    习题答案

--- 2/compounddecls.cc
+++ 2/compounddecls.cc
@@ -1,28 +1,31 @@
-
+// 2.3
 
 #include <iostream>
 int main()
 {
-	// i is an int; p is a pointer to int; r is a reference to int
-	int i = 1024, *p = &i, &r = i, &dummy = i; //i 可以有很多个不同的名字
+	// i is an int; p is a pointer to int; r is a reference to int, r and dummy are new names of i
+	int i = 1024, *p = &i, &r = i, &dummy = i; //
 	
 	// three ways to print the value of i
 	std::cout << i << " " <<  *p <<  " " << r << " r "<< dummy <<"= dummy"<< std::endl;
 
 	int j = 42, *p2 = &j;
 	std::cout << "int j = 42, *p2 = &j *&pref = p2 =" ;
+    // 完全相同的指针 *&
 	int *&pref = p2;  // bounding printers, pref is a reference to the pointer p2
 
 	// prints the value of j, which is the int to which p2 points
+    //42
 	std::cout << *pref <<" *pref"<< std::endl ;
 	
 	// pref refers to a pointer; assigning &i to pref makes p point to i
+    //1024
 	pref = &i; 
 	std::cout << *pref << std::endl; // prints the value of i
 
 	// dereferencing pref yields i, the int to which p2 points; 
 	*pref = 0;  // changes i to 0
-	
+	// i = 0
 	std::cout << i << " " << *pref << std::endl;
 
 	return 0;
--- 2/convs.cc
+++ 2/convs.cc
@@ -1,4 +1,4 @@
-
+//2.3.2
 
 #include <iostream>
 int main()
@@ -25,6 +25,23 @@ int main()
 	i = c;  // the character with value 255 is an unprintable character
 	        // assigns value of c (i.e., 255) to an int
 	std::cout << i << std::endl; // prints 255
-	
-	return 0;
+
+    double obj = 3.14, *pd = &obj;// ok: void* can hold the address value of any data pointer type
+    void *pv = &obj; // obj can be an object of any type
+    pv = pd;     // pv can hold a pointer to any type
+    std::cout << pv << std::endl; // prints 255
+    std::cout << pd << std::endl; // prints 255
+    pd+= 2*2*2*2*2*2*2*2 ;
+    std::cout << *pd << std::endl; // prints 255
+//Process finished with exit code 139 (interrupted by signal 11: SIGSEGV)
+//    int i = 42;
+    int &r = i;  // & follows a type and is part of a declaration; r is a reference
+    int *p;    // * follows a type and is part of a declaration; p is a pointer
+    p = &i;    // & is used in an expression as the address-of operator
+    *p = i;    // * is used in an expression as the dereference operator
+    int &r2 = *p; // & is part of the declaration; * is the dereference operator
+    std::cout << r2 << std::endl; // prints 255
+    std::cout << &r2 << std::endl; // prints 0x16d9ff588
+
+    return 0;
 }
